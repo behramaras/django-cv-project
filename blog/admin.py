25c641094111025c8386cb37b8fecbfa2ceb2f57
Behram Aras <behram.aras@gmail.com>
2026-08-19 12:34:25 +0100

    Add ExperienceAdmin with order field to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import About, Experience, Education, Certificate, Skill, Project, Interest
 
 admin.site.register(About)
-admin.site.register(Experience)
 admin.site.register(Education)
 admin.site.register(Certificate)
 admin.site.register(Skill)
